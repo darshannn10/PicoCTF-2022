5e4441a301e8b07237fcaf6a8347df991a016fcc
Darshan Patel <87711310+darshannn10@users.noreply.github.com>
2022-12-04 19:01:37 +0530

    Add files via upload

---
+++ forensics/sidechannel/suto.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+python3 side.py 00000000
+python3 side.py 10000000
+python3 side.py 20000000
+python3 side.py 30000000
+python3 side.py 40000000
+python3 side.py 50000000
+python3 side.py 60000000
+python3 side.py 70000000
+python3 side.py 80000000
+python3 side.py 90000000
